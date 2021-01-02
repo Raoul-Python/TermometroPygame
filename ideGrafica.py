8e2663311043c3855cc8ff1c27bbecd46d457231
Raoul-Python <76666021+Raoul-Python@users.noreply.github.com>
2021-01-02 23:16:43 +0100

    2621 Seguimos con el termómetro en Tkinter(), resulta interesante

--- ideGrafica.py
+++ ideGrafica.py
@@ -1,11 +1,13 @@
 from tkinter import *
-#from tkinter import ttk
+from tkinter import ttk
 
 
 
 class mainApp(Tk): #la clase mainApp hereda de la clase "padre" Tk()
 
     size = "200x350"
+    entrada = None
+    tipoUnidad = None
 
     def __init__(self):
         Tk.__init__(self)#Llama al constructor de Tk()
@@ -14,6 +16,20 @@ class mainApp(Tk): #la clase mainApp hereda de la clase "padre" Tk()
         self.title("TERMÓMETRO")
         self.configure(bg = "#DED7B9")
 
+        self.temperatura = StringVar(value="")
+        self.tipoUnidad = StringVar(value="C")
+
+        self.createLayout()
+
+    
+    def createLayout(self):
+
+        self.entrada = Entry(self, variable = self.temperatura)
+       
+
+
+
+
 
     def start(self):
         self.mainloop()
@@ -21,6 +37,7 @@ class mainApp(Tk): #la clase mainApp hereda de la clase "padre" Tk()
 if __name__ == "__main__":
 
     objeto = mainApp()
+    objeto.createLayout()
     objeto.start()
 
     
